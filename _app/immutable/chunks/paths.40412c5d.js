36080ea85c276d415f2a0d6e0f55c7d7a2c5b2a5
Vins <jourdan.v@orange.fr>
2023-08-19 17:22:24 +0200

    Updates

--- _app/immutable/chunks/paths.40412c5d.js
+++
@@ -1,1 +0,0 @@
-var s;const l=((s=globalThis.__sveltekit_1lxw8he)==null?void 0:s.base)??"/developer-portfolio";var e;const t=((e=globalThis.__sveltekit_1lxw8he)==null?void 0:e.assets)??l;export{t as a,l as b};
---
+++ _app/immutable/chunks/paths.558d2bc2.js
@@ -0,0 +1,1 @@
+var e;const t=((e=globalThis.__sveltekit_ktee1)==null?void 0:e.base)??"/developer-portfolio";var s;const a=((s=globalThis.__sveltekit_ktee1)==null?void 0:s.assets)??t;export{a,t as b};
