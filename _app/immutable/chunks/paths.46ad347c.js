3ab4316a86ae4534fcdb5a63083588d7b7f3c7c9
Vins <jourdan.v@orange.fr>
2023-08-21 13:50:38 +0200

    Updates

---
+++ _app/immutable/chunks/paths.46ad347c.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_a0d23a)==null?void 0:s.base)??"/developer-portfolio";var a;const t=((a=globalThis.__sveltekit_a0d23a)==null?void 0:a.assets)??e;export{t as a,e as b};
